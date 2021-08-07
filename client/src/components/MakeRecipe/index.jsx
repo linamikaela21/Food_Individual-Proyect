d59bc9ad1a4b58fbce4a2b4122e0faf5cfc14b29
Linamikaela21 <mikaelagutierrezarribas@gmail.com>
2021-08-06 21:38:52 -0700

    Formulario casi perfecto falta avisar usuario dietas marcadas

--- client/src/components/MakeRecipe/index.jsx
+++ client/src/components/MakeRecipe/index.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom";
 import "./index.css";
 
 import { addRecipe, getDiets } from "../../actions";
@@ -8,10 +8,13 @@ import { addRecipe, getDiets } from "../../actions";
 export function MakeRecipe() {
   //const {name, description, score, healthy, image, steps, diets} = req.body
 
-  const dispatch = useDispatch()
+  //Para redireccionarme una vez que envio el formulario
+  const history = useHistory();
+
+  const dispatch = useDispatch();
 
   //Me estoy trayendo el estado de diets
-  const diets = useSelector(state => state.diets)
+  const diets = useSelector((state) => state.diets);
 
   const [recipe, setRecipe] = useState({
     name: "",
@@ -24,108 +27,130 @@ export function MakeRecipe() {
   });
 
   useEffect(() => {
-    dispatch(getDiets())
+    dispatch(getDiets());
   }, [dispatch]);
 
   //Creo mi funcion onChange para el formulario para tener un estado general para todos mis inputs
-  // function onInputChange(e) {
-  //   setRecipe((prevState) => {
-  //     return {
-  //       ...prevState,
-  //       [e.target.name]: e.target.value,
-  //     };
-  //   });
-  // }
+  function onInputChange(e) {
+    setRecipe((prevState) => {
+      return {
+        ...prevState,
+        [e.target.name]: e.target.value,
+      };
+    });
+    console.log(e.target.value)
+  }
 
-  console.log(recipe);
-
-  function addDietsToRecipe(id) {
+  const handleDiets = (e) => {
     setRecipe({
       ...recipe,
-      diets: [...recipe.diets, id],
+      diets: [
+        ...recipe.diets, 
+        e.target.value]
     });
   }
 
   //Aca hago mi post a mi base de datos
-  async function handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(addRecipe(recipe))
+    dispatch(addRecipe(recipe));
     alert("Se ha creado una nueva receta");
-  }
+    setRecipe({
+      name: "",
+      description: "",
+      score: "",
+      healthy: "",
+      steps: "",
+      image: "",
+      diets: [],
+    });
+    history.push("/recipes");
+  };
 
   return (
     <div>
-    <Link to='/recipes'> <button>VOLVER</button> </Link>
-    <h1>CREA TU PROPIA RECETA !</h1>
-    <form onSubmit={handleSubmit}>
-      <p>
-        <label>Name:</label>
-        <input
-          type="text"
-          name="name"
-          value={recipe.name}
-          placeholder="Recipe Name"
-        />
-      </p>
-      <p>
-        <label>Description: </label>
-        <input
-          type="text"
-          name="description"
-          value={recipe.description}
-          placeholder="Recipe Description"
-        />
-      </p>
-      <p>
-        <label>Score:</label>
-        <input
-          type="number"
-          name="score"
-          value={recipe.score}
-          placeholder="Recipe Score"
-        />
-      </p>
-      <p>
-        <label>Healthy Score:</label>
-        <input
-          type="number"
-          name="healthy"
-          value={recipe.healthy}
-          placeholder="Recipe Healthy"
-        />
-      </p>
-      <p>
-        <label>Describe steps:</label>
-        <textarea
-          type="textarea"
-          name="steps"
-          value={recipe.steps}
-          placeholder="Recipe Steps"
-        />
-      </p>
-      <p>
-        <label>Load image </label>
-        <input
-          type="text"
-          name="image"
-          value={recipe.image}
-          placeholder="Recipe Image"
-        />
-      </p>
-      <div>
-        {diets.map((diet) => {
-          return (
-            <div>
-              {diet.name}
-              <button onClick={() => addDietsToRecipe(diet.id)}>
-                Agregar dieta
-              </button>
-            </div>
-          );
-        })}
-      </div>
-      <button type='submit'>Crear Receta</button>
-    </form>
+      <Link to="/recipes">
+        {" "}
+        <button>VOLVER</button>{" "}
+      </Link>
+      <h1>CREA TU PROPIA RECETA !</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <p>
+          <label>Name:</label>
+          <input
+            type="text"
+            name="name"
+            value={recipe.name}
+            placeholder="Recipe Name"
+            onChange={(e) => onInputChange(e)}
+          />
+        </p>
+        <p>
+          <label>Description: </label>
+          <input
+            type="text"
+            name="description"
+            value={recipe.description}
+            placeholder="Recipe Description"
+            onChange={(e) => onInputChange(e)}
+          />
+        </p>
+        <p>
+          <label>Score:</label>
+          <input
+            type="number"
+            name="score"
+            value={recipe.score}
+            placeholder="Recipe Score"
+            onChange={(e) => onInputChange(e)}
+          />
+        </p>
+        <p>
+          <label>Healthy Score:</label>
+          <input
+            type="number"
+            name="healthy"
+            value={recipe.healthy}
+            placeholder="Recipe Healthy"
+            onChange={(e) => onInputChange(e)}
+          />
+        </p>
+        <p>
+          <label>Describe steps:</label>
+          <textarea
+            type="textarea"
+            name="steps"
+            value={recipe.steps}
+            placeholder="Recipe Steps"
+            onChange={(e) => onInputChange(e)}
+          />
+        </p>
+        <p>
+          <label>Load image </label>
+          <input
+            type="text"
+            name="image"
+            value={recipe.image}
+            placeholder="Recipe Image"
+            onChange={(e) => onInputChange(e)}
+          />
+        </p>
+
+          <select onChange={e => handleDiets(e)}>
+            {
+            diets.map(diet => (
+              <option value={diet.name}>{diet.name}</option>
+            ))
+            }
+            <ul>
+              <li> {
+              recipe.diets.map(elem => elem + ", ")
+              } </li>
+            </ul>
+          </select>
+
+        <button type="submit">Crear Personaje</button>
+      </form>
     </div>
   );
 }
